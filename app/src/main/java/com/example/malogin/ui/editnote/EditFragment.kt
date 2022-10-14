119a0d54bbd3226036121c5aac4f2550cda3a0a5
Faisal Reza <7farema@gmail.com>
2022-10-15 05:55:06 +0700

    idk dialog fragment or something

--- app/src/main/java/com/example/malogin/ui/editnote/EditFragment.kt
+++ app/src/main/java/com/example/malogin/ui/editnote/EditFragment.kt
@@ -1,32 +1,64 @@
-package com.example.malogin.ui.editnote
-
-import androidx.lifecycle.ViewModelProvider
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.example.malogin.R
-
-class EditFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = EditFragment()
-    }
-
-    private lateinit var viewModel: EditViewModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_edit, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(EditViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
-
-}
+//package com.example.malogin.ui.editnote
+//
+//import android.content.Context
+//import android.content.SharedPreferences
+//import androidx.lifecycle.ViewModelProvider
+//import android.os.Bundle
+//import androidx.fragment.app.Fragment
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import android.widget.Toast
+//import androidx.databinding.DataBindingUtil
+//import androidx.fragment.app.DialogFragment
+//import androidx.fragment.app.viewModels
+//import androidx.navigation.fragment.findNavController
+//import androidx.navigation.fragment.navArgs
+//import com.example.malogin.R
+//import com.example.malogin.databinding.FragmentEditBinding
+//
+//class EditFragment : DialogFragment() {
+//    private var _binding : FragmentEditBinding? = null
+//    private val binding get() = _binding!!
+//
+//    private val args : EditNoteFragmentArgs by navArgs()
+//    private val viewModel : NoteViewModel by viewModels()
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_edit_note, container, false)
+//        return binding.root
+//    }
+//
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//
+//        val getData = args.dataNotes
+//        sharedPreferences = requireActivity().getSharedPreferences("registerData", Context.MODE_PRIVATE)
+//
+//        binding.apply {
+//            dataEdit = getData
+//            btnEdit.setOnClickListener {
+//                val title = binding.edtJudul.text.toString()
+//                val content = binding.edtCatatan.text.toString()
+//
+//                if (edtJudul.text!!.isEmpty() || edtCatatan.text!!.isEmpty()){
+//                    Toast.makeText(context, "Isi Note Dahulu", Toast.LENGTH_SHORT).show()
+//                }
+//                else{
+//                    viewModel.editNote(NoteData(args.dataNotes.id_notes, title, content))
+//                    Toast.makeText(context, "Edit Note tersimpan", Toast.LENGTH_SHORT).show()
+//                    findNavController().navigateUp()
+//                }
+//            }
+//        }
+//
+//    }
+//
+//    override fun onDestroyView() {
+//        super.onDestroyView()
+//        _binding = null
+//    }
+//}
